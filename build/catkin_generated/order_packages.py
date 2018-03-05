8f8c57a40cebd214ccc1cd8b5cb59417270cfa88
lovewen <Ghan6373@163.com>
2018-03-05 13:10:35 +0800

    second commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/root/ros/ros_test/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/root/ros/drrobot/arm/devel;/root/ros/drrobot/wheel/devel;/root/ros/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/root/ros/drrobot/arm/devel;/root/ros/drrobot/wheel/devel;/root/ros/catkin_ws/devel;/opt/ros/kinetic" != "" else []
